323c554fe3a5a49411a7c82fd8e87cb87802f05b
niikpakpobrown <brown.niikpakpo@gmail.com>
2023-08-12 18:29:53 +0000

    button renamed appropriately

--- ui.py
+++ ui.py
@@ -15,12 +15,12 @@ class App(tk.Tk):
         style.configure("Modern.TButton",font=("Helvetica", 16), background="#4CAF50", foreground="black", padding=10)
 
         # create themed results entry and printing buttons
-        self.btn = ttk.Button(self, text="Results Entry", style="Modern.TButton")
+        self.results_entry_btn= ttk.Button(self, text="Results Entry", style="Modern.TButton")
         self.print_results_btn = ttk.Button(self, text="Print Results", style="Modern.TButton")
         
         # place buttons at the centre of the window
-        self.btn.place(relx=0.5, rely=0.4, anchor="center")
-        self.btn2.place(relx=0.5, rely=0.6, anchor="center")
+        self.results_entry_btn.place(relx=0.5, rely=0.4, anchor="center")
+        self.print_results_btn.place(relx=0.5, rely=0.6, anchor="center")
         
 if __name__ == '__main__':
     app = App()
