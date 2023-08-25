fde2bd28487b29adfbcaf83b373d8d68476042f6
niikpakpobrown <brown.niikpakpo@gmail.com>
2023-08-25 18:49:51 +0000

    Properly comment on the lines

--- Lis_fxns.py
+++ Lis_fxns.py
@@ -1,6 +1,8 @@
+# Import the required libraries for GUI elements and database interaction
 import tkinter as tk
 from tkinter import ttk
 
+# Custom class for displaying and handling test elements in the UI
 class TestElement:
     def __init__(self, parent_frame, row, test_name, r_range, unit):
         self.parent_frame = parent_frame
@@ -13,6 +15,7 @@ class TestElement:
 
         self.create_widgets()
 
+    # Create and arrange widgets for displaying a test element
     def create_widgets(self):
         ttk.Label(self.parent_frame, text=self.test_name, font=("Helvetica", 12, "bold")).grid(row=self.row, column=0, padx=5, pady=5, columnspan=4)
         entry = ttk.Entry(self.parent_frame, font=("Helvetica", 12, "bold"))
@@ -21,14 +24,16 @@ class TestElement:
         ttk.Label(self.parent_frame, text=self.unit, font=("Helvetica", 12, "bold")).grid(row=self.row, column=8, padx=20, pady=5)
         ttk.Label(self.parent_frame, text=self.r_range, font=("Helvetica", 12, "bold")).grid(row=self.row, column=10, padx=10, pady=5, columnspan=2)
 
-
+    # Get the value entered in the test element's entry widget
     def get_value(self):
         return self.entry_widget.get()
-       
+    
+# Custom class for inserting test results into the database      
 class InsertResult:
     def __init__(self, db_connector):
         self.db_connector = db_connector
 
+    # Insert test result data into the database
     def insert_data(self, patient_id, patient_name, age, sex, test_date, t_bil, d_bil, alt, ast, alp, ggt, tp, alb):
         cursor= self.db_connector.cursor()
         cursor.execute("USE local_lis")
